be2e9101f2771e57ab79409c4a8f8538673afe9f
Chris Keating <christopherkeating@gmail.com>
2019-12-25 23:48:25 -0600

    Create brewtstrap.sh

---
+++ bin/brewtstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+ls -lad
+mkdir -p ~/.local/bin
+pushd ~/.local
+git clone https://github.com/Homebrew/brew
+pushd bin
+ln -s ../brew/bin/brew brew
+echo 'if [ -d "$HOME/.local/bin" ]; then export PATH="$PATH:$HOME/.local/bin"; fi' >> ~/.bash_profile
+bash -c 'brew install stow'
+brew install stow
