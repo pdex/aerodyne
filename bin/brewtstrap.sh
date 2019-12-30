3966392c3e8a6ec7848c49ee309936b9dcd473c0
Chris Keating <christopherkeating@gmail.com>
2019-12-30 09:28:50 -0600

    Update brewtstrap.sh

--- bin/brewtstrap.sh
+++ bin/brewtstrap.sh
@@ -1,6 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-ls -lad
+ls -la
+ls -la ~
+ls -la ~/.local
 mkdir -p ~/.local/bin
 pushd ~/.local
 git clone https://github.com/Homebrew/brew
@@ -8,4 +10,4 @@ pushd bin
 ln -s ../brew/bin/brew brew
 echo 'if [ -d "$HOME/.local/bin" ]; then export PATH="$PATH:$HOME/.local/bin"; fi' >> ~/.bash_profile
 bash -c 'brew install stow'
-brew install stow
+#brew install stow
