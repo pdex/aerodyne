b3a1c4ca3f5e7bf5918775e33c69bab51befb66f
Chris Keating <christopherkeating@gmail.com>
2019-12-30 16:28:15 -0600

    Update brewtstrap.sh

--- bin/brewtstrap.sh
+++ bin/brewtstrap.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 set -e -x
-ls -la
-ls -la ~
-ls -la ~/.local
+
 mkdir -p ~/.local/bin
 pushd ~/.local
 git clone https://github.com/Homebrew/brew
@@ -12,3 +10,10 @@ ln -s ../brew/bin/brew brew
 echo 'if [ -d "$HOME/.local/bin" ]; then export PATH="$PATH:$HOME/.local/bin"; fi' >> ~/.bash_profile
 bash -c 'brew install stow'
 #brew install stow
+popd
+popd
+mkdir .emacs.d
+touch .emacs.d/init.el
+bash -c 'stow -t $HOME -S .emacs.d'
+test -d $HOME/.emacs.d
+test -r $HOME/.emacs.d/init.el
